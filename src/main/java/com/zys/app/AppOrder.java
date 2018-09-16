1655e89868a6977846989f8fcc892b43ef07d4d7
ASUS <ASUS@DESKTOP-L3BECP7>
2018-09-16 23:40:59 +0800

    添加Hystrix的jar包

--- src/main/java/com/zys/app/AppOrder.java
+++ src/main/java/com/zys/app/AppOrder.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 /**
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+// @EnableHystrix //:开启Hystrix熔断器
 public class AppOrder {
 
 	public static void main(String[] args) {
--- src/main/java/com/zys/app/service/MemberService.java
+++ src/main/java/com/zys/app/service/MemberService.java
@@ -1,26 +1,38 @@
 package com.zys.app.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 /**
  * 调用service-member服务
  * @author ASUS
  *
  */
+
 @Service
 public class MemberService {
 	
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@SuppressWarnings("unchecked")
+	// @HystrixCommand(fallbackMethod = "orderError")  //:实现服务降级
 	public List<String> getOderInfoByUserList(){
-		
 		return  restTemplate.getForObject("http://service-member/getUserList", List.class);
 		
+	}
+	
+	
+	public List<String> orderError() {
+		List<String> listUser = new ArrayList<String>();
+		listUser.add("not orderUser list");
+		return listUser;
 	}
 
 }
